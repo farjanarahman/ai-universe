bd5cede8e7e41d44d07fc01ba61121aafcae0333
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-03-03 22:39:51 +0600

    data loaded"

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+
+const loadApps = async() => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    // displayApps(data.data.tools);
+}
