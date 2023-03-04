ac4fb343d3fe001b1fe446f5c9bd40cd3a1a9833
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-03-04 16:33:56 +0600

    The show all button working

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,29 @@
-
-const loadApps = async() => {
+const loadApps = async (dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/ai/tools`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
-  displayApps(data.data.tools);
-}
-const displayApps = apps => {
-  const appContainer = document.getElementById('app-container');
-  apps = apps.slice(0, 6);
+  displayApps(data.data.tools, dataLimit);
+};
 
-  apps.forEach(app => {
-      const appDiv = document.createElement('div');
-      appDiv.classList.add('col');
-      const featuresList = app.features.map((feature) => `<li>${feature}</li>`).join('');
-      appDiv.innerHTML = `
-        <div class="card p-4 rounded-3">
-          <img src="${app.image}" class="card-img-top rounded-3 h-100" alt="...">
+const displayApps = (apps, dataLimit) => {
+  const appContainer = document.getElementById("app-container");
+
+  const showAll = document.getElementById("show-all");
+  if (dataLimit && apps.length > dataLimit) {
+    apps = apps.slice(0, dataLimit);
+  }
+
+// clear previous content
+  appContainer.innerHTML = ""; 
+
+  apps.forEach((app) => {
+    const appDiv = document.createElement("div");
+    appDiv.classList.add("col");
+
+    const featuresList = app.features.map((feature) => `<li>${feature}</li>`).join("");
+    appDiv.innerHTML = `
+        <div class="card p-4">
+          <img src="${app.image}" class="card-img-top" alt="...">
           <div class="card-body">
             <h3 class="card-title fw-semibold">Features</h3>
             <ol class="card-text" type="1">${featuresList}</ol>              
@@ -33,9 +40,29 @@ const displayApps = apps => {
           </div>
         </div>
       `;
-      appContainer.appendChild(appDiv);
-    });
+    appContainer.appendChild(appDiv);
+  });
 };
 
-loadApps();
+const processShow = (dataLimit) => {
+  toggleSpinner(true);
+  loadApps(dataLimit);
+};
+
+document.getElementById("show-all").addEventListener("click", function () {
+  processShow();
+});
+
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById("loader");
+  if (isLoading) {
+    loaderSection.classList.remove("d-none");
+  } 
+  else {
+    loaderSection.classList.add("d-none");
+  }
+};
+
+// initially show only 6 cards
+loadApps(6); 
 
