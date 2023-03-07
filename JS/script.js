85265d82046657763c2ef2cc030687ecb9513bad
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-03-08 01:00:57 +0600

    Done

--- JS/script.js
+++ JS/script.js
@@ -27,8 +27,8 @@ const displayApps = (apps, dataLimit) => {
   // clear previous content
   appContainer.innerHTML = '';
 
+  //Cards
   apps.forEach((app) => {
-    console.log(app.description);
     const appDiv = document.createElement('div');
     appDiv.classList.add('col');
     const featuresList = app.features.map((feature) => `<li>${feature}</li>`).join('');
@@ -74,6 +74,7 @@ document.getElementById('show-all').addEventListener('click', function () {
   processShow();
 });
 
+//Start Loader
 const toggleSpinner = (isLoading) => {
   const loaderSection = document.getElementById('loader');
   if (isLoading) {
@@ -88,6 +89,7 @@ document.getElementById('btn-show-all').addEventListener('click', function () {
   processShow();
 });
 
+//Modal API
 const loadAppDetails = async(id) =>{
   id = ("0" + id).slice(-2);
   const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
@@ -96,8 +98,8 @@ const loadAppDetails = async(id) =>{
   displayAppDetails(data);
 }
 
+// Modal
 const displayAppDetails = app =>{
-  console.log(app.data);
   const modalBody = document.getElementById('my-modal')
   modalBody.innerHTML = ''
   const div = document.createElement('div')
@@ -108,40 +110,42 @@ const displayAppDetails = app =>{
               <div class="modal-left w-60">
                 <h3 class="app-des">${app.data.description}</h3>
                 <div class="subscibe d-flex fw-semibold mt-3">
-                  <div class="basic text-success">
-                    <p><span class="app-cost">${app.data.pricing ? app.data.pricing[0].price : 'Free of Cost'}</span> <span class="app-plan">${app.data.pricing[0].plan}</span></p>
+                  <div class="basic text-success p-1">
+                    <p><span class="app-cost">${app.data.pricing === null ? 'Free of Cost/': app.data.pricing[0].price }</span> <span class="app-plan">${app.data.pricing === null ? "Basic" : app.data.pricing[0].plan}</span></p>
                   </div>
-                  <div class="pro text-warning-emphasis">
-                    <p><span class="app-cost">${app.data.pricing ? app.data.pricing[1].price : 'Free of Cost'}</span> <span class="app-plan">${app.data.pricing[1].plan}</span>
+                  <div class="pro text-warning-emphasis p-1">
+                    <p><span class="app-cost">${app.data.pricing === null ? 'Free of Cost/' : app.data.pricing[1].price}</span> <span class="app-plan">${app.data.pricing === null ? "Pro" : app.data.pricing[1].plan}</span>
                       </p> 
                     </div>
-                    <div class="contact text-danger">
-                      <p> <span class="app-cost">${app.data.pricing ? app.data.pricing[2].price : 'Free of Cost'}</span> <span class="app-plan">${app.data.pricing[2].plan}</span>
+                    <div class="contact text-danger p-1">
+                      <p> <span class="app-cost">${app.data.pricing === null ? 'Free of Cost/' : app.data.pricing[2].price}</span> <span class="app-plan">${app.data.pricing === null ? "Enterprise" : app.data.pricing[2].plan}</span>
                       </p>
                      
                     </div>
                   </div>
-                  <div class="features-int d-flex">
-                    <div class="feature">
-                      <h3 class="int-head">Features</h3>
-                      <ol class=" text-center d-bl">${app.data.features ? Object.values(app.data.features).map(item=>`<li>${item.feature_name}</li>`).join(''): 'No data'}</ol>
-                    </div>
-                   <div class="int">
-                    <h3 class="int-head">Integrations</h3>
-                    <ol class=" text-center">${app.data.integrations ? Object.values(app.data.integrations).map(items=>`<li>${items}</li>`).join(''): 'No data'}</ol>
+                  <div class="features-int d-flex mt-3">
+                  <div class="feature">
+                    <h3 class="int-head">Features</h3>
+                    <ul class="text">${app.data.features !=null ? Object.values(app.data.features).map(item=>`<li>${item.feature_name}</li>`).join('') : 'No data found'}</ul>
                   </div>
-                  </div>
+                 <div class="int">
+                  <h3 class="int-head">Integrations</h3>
+                  <ul class="text">${app.data.integrations !=null ? Object.values(app.data.integrations).map(items=>`<li>${items}</li>`).join(''): 'No data found'}</ul>
                 </div>
-
-              <div class="modal-right ms-2">
-                 <div class="card-wrapper">
-                  <img src="${app.data.image_link[0]}" class="card-img card-img-top app-img rounded" alt="...">
-                  <p class="accuracy">${accurate}% accuracy</p>
-                 </div>
-
-                <h4 class="input mt-3">${app.data.input_output_examples[0].input}</h4>
-                <p>${app.data.input_output_examples[0].output}</p>
-              </div>
+                </div>
+                </div>
+                
+                <div class="modal-right ms-2">
+                <div class="card-wrapper">
+                <img src="${app.data.image_link[0]}" class="card-img card-img-top app-img rounded" alt="...">
+                
+                ${accurate > 0 ? `<p class="accuracy">${accurate}% accuracy</p>` : ""}
+                
+                </div>
+                
+                <h4 class="input mt-3">${app.data.input_output_examples !=null ?app.data.input_output_examples[0].input : "Can you give any example?" }</h4>
+                <p>${app.data.input_output_examples !=null ? app.data.input_output_examples[0].output : "No! Not Yet! Take a break!!!" }</p>
+                </div> 
             </div>
             `
             modalBody.appendChild(div)
